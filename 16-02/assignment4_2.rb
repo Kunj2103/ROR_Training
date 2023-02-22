611851c32134d1004925f4b1f8fd6d0ed8afec43
Kunj2103 <74104025+Kunj2103@users.noreply.github.com>
2023-02-22 10:57:29 +0530

    Update assignment4_2.rb

--- 16-02/assignment4_2.rb
+++ 16-02/assignment4_2.rb
@@ -1,37 +1,29 @@
 class Calculator
-    puts "Enter First operand"
-    @operand1 = gets.chomp.to_i
-    puts "Enter Second operand"
-    @operand2 = gets.chomp.to_i
+  puts "Enter First operand"
+  @operand1 = gets.chomp.to_i
+  puts "Enter Second operand"
+  @operand2 = gets.chomp.to_i
 
-    puts "What operation you want to perform"
-    puts "1. Sum"
-    puts "2. Subtract"
-    puts "3. Multiply"
-    puts "4. Division"
+  puts "What operation you want to perform"
+  puts "1. Sum"
+  puts "2. Subtract"
+  puts "3. Multiply"
+  puts "4. Division"
 
-    @option = gets.chomp.to_i;
-    case 
-    when @option == 1
-        sum = Proc.new {|op1,op2| op1+op2}
-        puts sum.call(@operand1,@operand2)
-        # sum(@operand1,@operand2)
-    when  @option == 2
-        sum = Proc.new {|op1,op2| op1-op2}
-        puts sum.call(@operand1,@operand2)
-
-        # subtract(@operand1,@operand2)
-    when  @option == 3
-        sum = Proc.new {|op1,op2| op1*op2}
-        puts sum.call(@operand1,@operand2)
-
-        # multiply(@operand1,@operand2)
-    when @option == 4
-        sum = Proc.new {|op1,op2| op1/op2}
-        puts sum.call(@operand1,@operand2)
-
-        # division(@operand1,@operand2)
-    
-    end
+  @option = gets.chomp.to_i;
+  case 
+  when @option == 1
+    sum = Proc.new {|op1,op2| op1+op2}
+    puts sum.call(@operand1,@operand2)
+  when  @option == 2
+    sum = Proc.new {|op1,op2| op1-op2}
+    puts sum.call(@operand1,@operand2)
+  when  @option == 3
+    sum = Proc.new {|op1,op2| op1*op2}
+    puts sum.call(@operand1,@operand2)
+  when @option == 4
+    sum = Proc.new {|op1,op2| op1/op2}
+    puts sum.call(@operand1,@operand2)    
+  end
 
 end
