6816ad47265e0c7270880486ca3c3447abb08f52
Kunj2103 <74104025+Kunj2103@users.noreply.github.com>
2023-02-23 18:18:01 +0530

    Create new method for user_info.
    Also make the variable as a local variable in billing_info method

--- 13-04/assignment1_1.rb
+++ 13-04/assignment1_1.rb
@@ -4,23 +4,25 @@ class Product
   def initialize(name,price)
     @productName=name
     @productPrice=price
-    puts "Enter your name"
+  end
+  
+  def user_info
+  	puts "Enter your name"
     @userName = gets.chomp
     puts "Enter GST"
     @gst = gets.chomp.to_i
   end
   
-  def get_user
+  def billing_info
     puts "Welcome to the #{SHOP_NAME}"
     puts "Welcome #{@userName}. Your invoice with #{@gst}% gst are below"
     puts "product details"
-    @gstInRps = @gst * @productPrice / 100
-    @total = @gstInRps + @productPrice
-    puts "#{@productName} - #{@total}"
+    gstInRps = @gst * @productPrice / 100
+    total = gstInRps + @productPrice
+    puts "#{@productName} - #{total}"
   end
 end
 
 p1 = Product.new("#{$company_name} laptop",220000)
-p1.get_user()
-
-
+p1.user_info
+p1.billing_info
